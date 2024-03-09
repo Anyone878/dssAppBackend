81d277d74222e3249d508be564a74d523f2c65b0
Yubo Wu <yubo.w5@outlook.com>
2024-03-09 16:19:01 +0000

    add GET user (2)

--- src/main/java/org/anyone/backend/controller/UserController.java
+++ src/main/java/org/anyone/backend/controller/UserController.java
@@ -6,6 +6,9 @@ import org.anyone.backend.repository.UserRepository;
 import org.anyone.backend.util.ResponseData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +27,7 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseData<?> getUser(@RequestBody JsonNode requestBody) {
         int userID = requestBody.get("id").asInt();
         if (userID == 0) {
@@ -32,8 +35,19 @@ public class UserController {
         }
         Optional<Users> userOptional = userRepository.findById(userID);
         if (userOptional.isEmpty()) {
-            return new ResponseData<>(404, "user not found", null);
+            return new ResponseData<>(404, "user not found in get (1)", null);
         }
-        return new ResponseData<Users>(200, "user found", userOptional.get());
+        return new ResponseData<>(200, "user found", userOptional.get());
+    }
+
+    @GetMapping()
+    public ResponseData<?> getUser(
+            @CurrentSecurityContext(expression = "authentication.principal") UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        Optional<Users> usersOptional = userRepository.findUserByUsername(username);
+        if (usersOptional.isEmpty()) {
+            return new ResponseData<>(404, "user not found in get (2)", null);
+        }
+        return new ResponseData<>(200, "user found", usersOptional.get());
     }
 }
